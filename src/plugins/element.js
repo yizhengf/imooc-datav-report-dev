1f34b1dda1055720bb199cc231bb9be77b9a76e8
yizhengf <yizhengf611@gmail.com>
2022-10-11 09:22:57 +0800

    map is undefined error， mapstyle cannot be applied

---
+++ src/plugins/element.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import { Card, Row, Col, Menu, MenuItem, RadioButton, RadioGroup, DatePicker , Table, TableColumn, Pagination } from 'element-ui'
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioButton)
+Vue.use(RadioGroup)
+Vue.use(DatePicker)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Pagination)
